3efe4b7449cdfe77d503fd05f5d6989b2657f450
Your Name <you@example.com>
2026-04-04 11:35:55 -0300

    fix: reversão de expiração do token (exp) para UTC para garantir login estável

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,5 +1,5 @@
 import os
-from datetime import timedelta
+from datetime import datetime, timedelta
 from .utils import get_brasilia_time
 from typing import Optional
 from jose import jwt
@@ -25,10 +25,10 @@ def get_password_hash(password: str) -> str:
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = get_brasilia_time() + expires_delta
+        expire = datetime.utcnow() + expires_delta
     else:
         # Default expiration
-        expire = get_brasilia_time() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
